be63bd7db549ddcacdc2b7889b0ed32f5fbc5817
Darya <kokov.dara.8104@gmail.com>
2024-11-21 01:02:12 +0300

    ботик

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import sqlite3
 from flask_cors import CORS
+import requests 
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://duudarra.github.io"}})
@@ -10,6 +11,25 @@ def get_db_connection():
     conn = sqlite3.connect('contact_form.db')
     conn.row_factory = sqlite3.Row  # Это позволит обращаться к столбцам по имени
     return conn
+
+def send_telegram_message(message):
+    token = '7711517174:AAEADDphW2twQEg7Zpy30NfI3qf4W_2urF0'  # Замените на ваш токен
+    chat_id = '747742170'  # Замените на ваш chat_id
+    
+    # Формирование URL для отправки сообщения
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    
+    # Отправка POST запроса в Telegram
+    response = requests.post(url, data=params)
+    
+    if response.status_code != 200:
+        print("Ошибка при отправке сообщения в Telegram")
+    else:
+        print("Сообщение успешно отправлено в Telegram")
 
 # Эндпоинт для получения всех записей
 @app.route('/responces', methods=['GET'])
@@ -36,6 +56,9 @@ def add_response():
     cursor.execute("INSERT INTO messages (name, email, message) VALUES (?, ?, ?)", (name, email, message))
     conn.commit()
     conn.close()
+
+    telegram_message = f"Новое сообщение:\nИмя: {name}\nEmail: {email}\nСообщение: {message}"
+    send_telegram_message(telegram_message)  # Отправка сообщения в Telegram
 
     return jsonify({'message': 'Response added successfully!'}), 201
 
