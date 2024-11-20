281b33c256c2315755131650df8aa387b9aea890
Darya <kokov.dara.8104@gmail.com>
2024-11-20 09:22:38 +0300

    as

---
+++ create_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+# Подключение к базе данных (если файла нет, он будет создан)
+conn = sqlite3.connect('contact_form.db')
+cursor = conn.cursor()
+
+# Создание таблицы для хранения данных
+cursor.execute('''CREATE TABLE IF NOT EXISTS responses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    message TEXT NOT NULL
+)''')
+
+# Сохранение изменений и закрытие соединения
+conn.commit()
+conn.close()
